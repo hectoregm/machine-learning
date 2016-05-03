5d21b496f45adb65de26fc156aca53e6967b7993
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-05-02 22:29:10 -0500

    Finished week 7 exercises.

--- week7/ex6/dataset3Params.m
+++ week7/ex6/dataset3Params.m
@@ -23,11 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+Space = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+results = zeros(length(Space), length(Space));
+min_cost = 10000;
+min_pair = [0; 0];
 
-
-
-
-
+for i = 1:length(Space)
+  for j = 1:length(Space)
+    c = Space(i);
+    sig = Space(j);
+    model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sig));
+    predictions = svmPredict(model, Xval);
+    results(i,j) = mean(double(predictions ~= yval));
+    if (results(i,j) < min_cost)
+      C = c;
+      sigma = sig;
+      min_cost = results(i,j);
+    end 
+  end
+end
 
 % =========================================================================
 
